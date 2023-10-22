0ba90e942bd2de082cec22f5a4447be2b13f2fba
claudio biselli <claudio@MacBook-Air-di-Laura.local>
2023-10-22 18:12:59 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Layout from "./components/Layout";
+
+function App() {
+  return (
+    <main>
+      <Layout />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/consts.ts
@@ -0,0 +1,4 @@
+export const navigation = [
+  { name: "Dashboard", href: "#" },
+  { name: "Reports", href: "#" },
+];
